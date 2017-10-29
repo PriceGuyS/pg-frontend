c74827b28fdccfb04471f85365f38968b724aa35
DavisFrench <davisdavis27@gmail.com>
2017-10-29 11:29:52 -0400

    Query is timing out. Table being used is ListingsTest which has 300 entries so I'm p sure the code is wrong. Tried a lot of different tutorials

---
+++ queryTest.js
@@ -0,0 +1,27 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('../.aws/config.json');
+
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+AWS.config.update({endpoint: "https://dynamodb.us-west-2.amazonaws.com"});
+
+console.log("hello");
+
+var params = {
+  ExpressionAttributeValues: {
+    ':hope': '332412832140'
+  },
+  KeyConditionExpression: 'id = :hope',
+  TableName : "ListingsTest"
+  // KeyConditionExpression: "#id = :id",
+  // ExpressionAttributeNames:{
+  //   "#id": "id"
+  // },
+  // ExpressionAttributeValues:{
+  //   ":id": 332412832140
+  // }
+};
+
+docClient.query(params, function(err, data) {
+    if (err) console.log(err); // an error occurred
+    else console.log(data.Item); // successful response
+});
