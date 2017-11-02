df753f0fc6a55596c6e60b652c391b40f6bbe52c
DavisFrench <davisdavis27@gmail.com>
2017-11-02 17:44:21 -0400

    Non functioning since it requires a different authentication method using aws.cognitoidentitycredentials will look into later

--- src/Query.js
+++ src/Query.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Query.css';
+//import './queryTest.js';
+//var test = require('./queryTest.js');
 
 class Query extends Component {
   render() {
     return (
       <div className="Query">
 				<header className="Query-header">
-					<div id="Query" className="tabcontent">					
+					<div id="Query" className="tabcontent">
 						<h1 className="Query-title">Query$</h1>
 						<h3>QUERY</h3>
 						<p>Query me Bro!</p>
