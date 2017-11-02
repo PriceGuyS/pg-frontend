c40098e87b22d793a2aeb88ed1c6686587b3990d
DavisFrench <davisdavis27@gmail.com>
2017-11-02 17:06:49 -0400

    Successfully return results. I haven't messed with filters, but this is a good code base.

--- queryTest.js
+++ queryTest.js
@@ -5,23 +5,26 @@ var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 AWS.config.update({endpoint: "https://dynamodb.us-west-2.amazonaws.com"});
 
 console.log("hello");
+//search('Super Mario 64')
+search('Banjo Kazooie')
 
-var params = {
-  ExpressionAttributeValues: {
-    ':hope': '332412832140'
-  },
-  KeyConditionExpression: 'id = :hope',
-  TableName : 'ListingsTest'
-  // KeyConditionExpression: "#id = :id",
-  // ExpressionAttributeNames:{
-  //   "#id": "id"
-  // },
-  // ExpressionAttributeValues:{
-  //   ":id": 332412832140
-  // }
-};
+function search(input){
+  var params = {
+    ExpressionAttributeValues: {
+      ':test': input
+      //':q': 'query',
+      //':hope': 'r',
+      //':end': 't',
+      //':sub': 'Super Mario 64',
+      //':blah': '332412832140',
+    },
+    KeyConditionExpression: 'inputQuery = :test', //has to be = and it has to be an exact match
+    //FilterExpression: 'contains (:q, :sub)',
+    TableName : 'testTable6' // this table has the proper key structure
+  };
 
-docClient.query(params, function(err, data) {
-    if (err) console.log(err); // an error occurred
-    else console.log(data); // successful response
-});
+  docClient.query(params, function(err, data) {
+      if (err) console.log(err); // an error occurred
+      else console.log(data); // successful response
+  });
+}
