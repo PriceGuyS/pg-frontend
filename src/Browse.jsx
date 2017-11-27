a97a80f399d3b0b3f89bd2ebcacdca87a3ca8be0
DERIK THOMPSON <derik.thompson82@gmail.com>
2017-11-26 23:58:57 -0500

    added Browse.jsx

---
+++ src/Browse.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Browse.css';
+//import './queryTest';
+//var test = require('./queryTest');
+
+class Browse extends Component {
+  render() {
+    return (
+      <div className="Browse">
+				<header className="Browse-header">
+					<div id="Browse" className="tabcontent">
+						<h1 className="Browse-title">Browse$</h1>
+						<h3>QUERY</h3>
+						<p>Browse me Bro!</p>
+					</div>
+				</header>
+			</div>
+    );
+	}
+}
+export default Browse;
