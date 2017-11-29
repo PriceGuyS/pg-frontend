a85bb37e3d345ebde74baf83fa1fb2542fb83d17
DavisFrench <davisdavis27@gmail.com>
2017-11-28 19:23:14 -0500

    ordered and sliced. Now returns an array, which I'm having trouble getting to react app.

--- src/queryTest.js
+++ src/queryTest.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk');
 
 AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: '####', //put the key here
+    IdentityPoolId: 'us-east-1:1ebfaeaa-5d62-4e67-ae99-b7d2a407c1e8', //put the key here
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
@@ -10,18 +10,20 @@ var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 function compare(data) {
   console.log("yo");
   console.log(typeof data); //object
-  console.log(data.Items[0].price); //how to access //https://gist.github.com/umidjons/9614157
+  //console.log(data.Items[0].price); //how to access
   data.Items.sort(function(a, b){
-    return parseFloat(a.price) - parseFloat(b.price);
+    return parseFloat(a.price) - parseFloat(b.price); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
   });
 
-  console.log(data.Items);
+  //console.log(data.Items);
+  //console.log(data.Items.slice(0,10));
+  return(data.Items.slice(0,10)) //pass a parameter for slice, will need try and except for key errors, in which case return all we have
 
 }
 //http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
-search("Super Mario 64")
+//search("Super Mario 64")
 
-function search(input) {
+module.exports = function search(input) {
   var params = {
     ExpressionAttributeValues: {
       ':test': input
@@ -33,6 +35,10 @@ function search(input) {
 
   return docClient.query(params, function(err, data) {
       if (err) return err; // an error occurred
-      else compare(data);
+      else{
+        var results = compare(data);
+        console.log(typeof results); //prints 10
+        return(results);
+      }
   });
 }
