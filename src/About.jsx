c976c3c9cd28f98ea87ea940b409a80d94c53f40
DERIK THOMPSON <derik.thompson82@gmail.com>
2017-12-03 16:58:33 -0500

    Added group photo to About

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import photo from './images/group_photo.png';
 import './About.css';
 
 class About extends Component {
   render() {
     return (
       <div className="About">	
-					<p>About me Bro!</p>
+					<pheader className = "About-pheader">
+					  <p>The Guys!</p>
+						<img src={photo} className="Group-photo" alt="group_photo" />
+					</pheader>
 			</div>
     );
 	}
