71f7e2e34e116bf77213ef91b65409f123cfb12f
John Le <johnminhle01@gmail.com>
2017-12-03 13:35:20 -0500

    Updated home page withs earch button. probably going to create a SearchButton component

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('LUL SAME: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return(
-      <div className="Home">	
+      <div className="Home">
 					<p>HOME is where the heart is, and the heart is in gaming.</p>
 					<p className="Home-intro">
 						To get started, click Query and submit your choices.
 					</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Search for games" value={this.state.value} onChange={this.handleChange} className="input-field"/>
+            <input type="submit" value="Search" id="submit-button"/>
+          </form>
 			</div>
     );
 	}
