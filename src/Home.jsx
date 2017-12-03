cb7c2299da03aabd7e2d11e7a20a4c8c40c121f2
John Le <johnminhle01@gmail.com>
2017-12-03 16:04:12 -0500

    added placeholder cards for items in home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Home.css';
 import SearchBar from './SearchBar';
+import { Container, Row, Col, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Home extends Component {
   constructor(props) {
@@ -28,6 +30,43 @@ class Home extends Component {
 						To get started, click Query and submit your choices.
 					</p>
           <SearchBar />
+          <Container className="items-container">
+          <Row>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
 			</div>
     );
 	}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 
