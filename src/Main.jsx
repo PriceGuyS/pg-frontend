930e96a545b9299352014c0f4096dfbfb0b5b7ba
John Le <johnminhle01@gmail.com>
2017-12-04 06:20:37 -0500

    Refactored the Route definitions

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Main.css';
 import Menu from './Menu';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Browse from './Browse';
+import About from './About';
 
 class Main extends Component {
   render() {
     return (
-      <div className="Main">
-				<header className="Main-header">
-					<img src={logo} className="Main-logo" alt="logo" />
-					<h1 className="Main-title">Welcome to PriceGuy$</h1>
-				</header>
-				<Menu/>
-			</div>
+      <Router>
+        <div className="Main">
+				    <header className="Main-header">
+					       <img src={logo} className="Main-logo" alt="logo" />
+					       <h1 className="Main-title">Welcome to PriceGuy$</h1>
+				    </header>
+				    <Menu/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/browse" component={Browse}/>
+            <Route path="/about" component={About}/>
+		   </div>
+     </Router>
     );
 	}
 }
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import './Menu.css';
-import Home from './Home';
-import Browse from './Browse';
-import About from './About';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Menu extends Component {
   render() {
     return(
-      <Router>
         <div>
           <button className="Menu-button" id="Home"><Link to="/" style={{ textDecoration: 'none', color: "white" }}>Home</Link></button>
           <button className="Menu-button" id="Browse"><Link to="/browse" style={{ textDecoration: 'none', color: "white" }}>Browse</Link></button>
           <button className="Menu-button" id="About"><Link to="/about" style={{ textDecoration: 'none', color: "white" }}>About</Link></button>
-
-          <Route exact path="/" component={Home}/>
-          <Route path="/browse" component={Browse}/>
-          <Route path="/about" component={About}/>
         </div>
-      </Router>
     );
   }
 };
